0f9afc59e3df85773f15c03bcf8df42e9e2964c0
Martin <mapmoeu@gmail.com>
2018-09-17 22:34:42 +0300

    big4 implemented
    will add in-depth description of my conception in ReadMe.txt, after dinner

--- Lists/ListBasedClasses/List2.h
+++ Lists/ListBasedClasses/List2.h
@@ -3,19 +3,18 @@
 template <class T>
 struct linkedData
 {
-	linkedData(const T&, const int, linkedData* = nullptr, linkedData* = nullptr);
-	linkedData(const linkedData&) = delete;
+	linkedData(const T&, linkedData* = nullptr, linkedData* = nullptr);
+	linkedData(const linkedData&)=delete;
 	linkedData&operator=(const linkedData&) = delete;
 	~linkedData();
 	T m_data;
-	const int m_key;
 	linkedData * m_next;
 	linkedData * m_prev;
 };
 
 template<class T>
-inline linkedData<T>::linkedData(const T & data, const int key, linkedData * next, linkedData * prev) 
-	: m_data(data), m_key(key), m_next(next), m_prev(prev)
+inline linkedData<T>::linkedData(const T & data, linkedData * next, linkedData * prev) 
+	: m_data(data), m_next(next), m_prev(prev)
 {
 }
 
@@ -32,4 +31,73 @@ inline linkedData<T>::~linkedData()
 		m_prev->m_next = nullptr;
 		delete m_prev;
 	}
+}
+
+template <class T>
+class List2
+{
+public:
+	List2();
+	List2(const T&);
+	List2(const List2<T>&);
+	List2<T>& operator=(const List2<T>&);
+	~List2();
+
+
+	//Modifiers
+	void push_back(const linkedData<T>&);
+private:
+	linkedData<T> * m_Begin;
+	linkedData<T> * m_End;
+};
+
+template<class T>
+inline List2<T>::List2() :  m_Begin(nullptr), m_End(nullptr)
+{
+}
+
+template<class T>
+inline List2<T>::List2(const T & val) : m_Begin(new linkedData<T>(val)), m_End(this->m_Begin)
+{
+}
+
+template<class T>
+inline List2<T>::List2(const List2<T>& rhs) : m_Begin(new linkedData<T>(rhs.m_Begin->m_data)), m_End(this->m_Begin)
+{
+	linkedData<T> * tmp = rhs.m_Begin;
+	while (tmp->m_next != rhs.m_End)
+	{
+		tmp = tmp->m_next;
+		push_back(*tmp);
+	}
+}
+
+template<class T>
+inline List2<T> & List2<T>::operator=(const List2<T> & rhs)
+{
+	if (this != &rhs)
+	{
+		delete this->m_Begin;
+		linkedData<T> * tmp = rhs.m_Begin;
+		while (tmp->m_next != rhs.m_End)
+		{
+			tmp = tmp->m_next;
+			push_back(*tmp);
+		}
+	}
+	return *this;
+}
+
+template<class T>
+inline List2<T>::~List2()
+{
+	delete this->m_Begin;
+}
+
+template<class T>
+inline void List2<T>::push_back(const linkedData<T>&rhs)
+{
+	this->m_End->m_next = new linkedData<T>(rhs.m_data);
+	this->m_End->m_next->m_prev = this->m_End;
+	this->m_End = this->m_End->m_next;
 }
